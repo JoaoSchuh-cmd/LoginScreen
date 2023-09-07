bbca785550bcb5e68769a01f5eeb0c3487f0f731
João Schuh <82126946+JoaoSchuh-cmd@users.noreply.github.com>
2023-09-07 15:54:17 -0300

    Commiting Project

---
+++ LoginScreen/LoginScreen/ContentView.swift
@@ -0,0 +1,121 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var edEmail = ""
+    @State private var edPassword = ""
+    @State private var isOn = false
+    
+    var body: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+                VStack(alignment: .leading) {
+                    Text("Crie sua conta")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.top)
+                    
+                    VStack(alignment: .leading) {
+                        Text("E-mail")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundColor(.secondary)
+                        TextField("", text: $edEmail)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }.padding(.top)
+         
+                    VStack(alignment: .leading) {
+                        HStack{
+                            Text("Senha")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                            Spacer()
+                            Button(action: {
+                                
+                            }) {
+                                Text("Recuperar senha")
+                            }
+                        }
+                        SecureField("", text: $edPassword)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                    }.padding(.top)
+                    Toggle(isOn: $isOn) {
+                        Text("Lembrar minha senha")
+                    }
+         
+                    Button(action: {
+                        
+                    }) {
+                        Text("Criar conta")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.black)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }.padding(.top)
+                    
+                    Button(action: {
+                        
+                    }) {
+                        HStack {
+                            Image(systemName: "applelogo")
+                                .foregroundColor(Color.black)
+                            
+                            Text("Continuar com Apple")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .padding()
+                            
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .border(Color.black, width: 5)
+                        .cornerRadius(10)
+                    }.padding(.top)
+                    
+                    HStack {
+                        Spacer()
+                        Text("Já possui uma conta?")
+                            .font(.headline)
+                            .foregroundColor(.secondary)
+
+                        Button(action: {
+
+                        }) {
+                            Text("Entre")
+                                .fontWeight(.bold)
+                        }
+                        Spacer()
+                    }
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(20)
+                .padding()
+                
+                Spacer()
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginScreen/LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by user241342 on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
